c427b84e006d9c283474d324d2c70755636f667a
Amir MOHAMMADI <amir.mohammadi@idiap.ch>
2020-11-10 11:40:24 +0100

    Sphinx 3 autodoc no longer accepts autodoc_default_flags and autodoc_default_options should be used

--- doc/conf.py
+++ doc/conf.py
@@ -211,11 +211,11 @@ rst_epilog = """
 # Default processing flags for sphinx
 autoclass_content = 'class'
 autodoc_member_order = 'bysource'
-autodoc_default_flags = [
-  'members',
-  'undoc-members',
-  'show-inheritance',
-  ]
+autodoc_default_options = {
+  "members": True,
+  "undoc-members": True,
+  "show-inheritance": True,
+}
 
 # For inter-documentation mapping:
 from bob.extension.utils import link_documentation, load_requirements
@@ -229,23 +229,4 @@ else:
   intersphinx_mapping = link_documentation()
 
 
-# We want to remove all private (i.e. _. or __.__) members
-# that are not in the list of accepted functions
-accepted_private_functions = ['__array__']
-
-def member_function_test(app, what, name, obj, skip, options):
-  # test if we have a private function
-  if len(name) > 1 and name[0] == '_':
-    # test if this private function should be allowed
-    if name not in accepted_private_functions:
-      # omit privat functions that are not in the list of accepted private functions
-      return skip
-    else:
-      # test if the method is documented
-      if not hasattr(obj, '__doc__') or not obj.__doc__:
-        return skip
-  return False
-
-def setup(app):
-  app.connect('autodoc-skip-member', member_function_test)
 

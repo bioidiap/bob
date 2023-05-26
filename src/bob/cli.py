b7f2f468afb6a0f65d5bc835a382f6d27dee35f4
Yannick DAYER <yannick.dayer@idiap.ch>
2023-05-26 14:47:18 +0200

    fix(cli): Clean entry-points up and add bob config

--- src/bob/cli.py
+++ src/bob/cli.py
@@ -5,17 +5,30 @@ from importlib.metadata import entry_points
 import clapper.logging
 import click
 
-from clapper.click import AliasedGroup
+from clapper.rc import UserDefaults
+from clapper.click import AliasedGroup, user_defaults_group
 from click_plugins import with_plugins
 
 logger = clapper.logging.setup("bob")
-
 
 @with_plugins(entry_points(group="bob.cli"))
 @click.group(
     cls=AliasedGroup,
     context_settings={"help_option_names": ("-?", "-h", "--help")},
 )
-def bob():
+def bob_main_cli():
     """The main command line interface for bob."""
+    # An entry-point `bob` is created at the package level pointing here.
+    #
+    # Packages that want to insert their sub-command in bob have to create an
+    # entry-point in the `bob.cli` group in their pyproject.toml:
+    #   [project.entry-points."bob.cli"]
+    #       custom = "bob.custom.cli:custom_command"
     pass
+
+@user_defaults_group(logger=logger, config=UserDefaults("bobrc.toml", logger=logger))
+def bob_config(**kwargs):
+    "Allows reading and writing into the user configuration."
+    pass
+
+bob_main_cli.add_command(bob_config, "config")

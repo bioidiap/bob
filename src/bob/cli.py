38ec2b1ceffd6ab5f7dfa5cfaa5b907be4ec9f13
Yannick DAYER <yannick.dayer@idiap.ch>
2023-05-26 11:32:08 +0200

    fix: Use clapper for the click options and groups.

--- src/bob/cli.py
+++ src/bob/cli.py
@@ -1,12 +1,11 @@
-"""Sets up the click plugin interface for packages to plug into."""
-
-import click
+"""Sets up the click plugin group for packages to plug into."""
 
 from importlib.metadata import entry_points
 
 import clapper.logging
+import click
 
-from bob.click_helper import AliasedGroup
+from clapper.click import AliasedGroup
 from click_plugins import with_plugins
 
 logger = clapper.logging.setup("bob")
